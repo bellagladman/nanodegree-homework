1a37563b42a9f3aa5050c3424a6de8439ee14949
Isobel (Bella) Gladman <isobelgladman@gmail.com>
2021-10-28 17:22:42 +0100

    Modified ATM program to create functions for setting correct pin and setting account balance, and then adjusting other functions accordingly

--- task_2_atm_program.py
+++ task_2_atm_program.py
@@ -23,14 +23,14 @@ then you need to raise an error and exit the program.
 """
 
 
-# Set account balance
-account_balance = 100
-
 # Set correct PIN
-correct_pin = 1234
+def set_correct_pin(pin):
+    correct_pin = pin
+    # print('Correct pin set to: {}'.format(correct_pin))
+    return correct_pin
 
 
-def check_pin_is_correct():
+def check_pin_is_correct(correct_pin):
     pin_entry = int(input('Please type in your PIN: '))
     count = 1
     if pin_entry == correct_pin:
@@ -49,24 +49,35 @@ def check_pin_is_correct():
                 return True
 
 
-def withdraw_cash(balance):
+# Set account balance
+def set_account_balance(balance):
+    account_balance = balance
+    # print('Account balance set to: £{}'.format(account_balance))
+    return account_balance
+
+
+def withdraw_cash(account_balance):
     try:
         withdrawal_request = int(input('How much money would you like to withdraw? '))
         assert 0 <= withdrawal_request, "You can't withdraw a negative number. Please try again."
-        assert withdrawal_request <= balance, "Insufficient funds. Please try again."
+        assert withdrawal_request <= account_balance, "Insufficient funds. Please try again."
     except Exception as e:
         print(e)
     else:
-        account_balance = balance - withdrawal_request
+        account_balance = account_balance - withdrawal_request
         print("Your request for £{:.2f} has been accepted. Please take your cash.".format(withdrawal_request))
         print("Your new account balance is: £{:.2f}.".format(account_balance))
 
-def make_a_withdrawal():
-    if check_pin_is_correct():
+
+def make_a_withdrawal(correct_pin, account_balance):
+    if check_pin_is_correct(correct_pin):
         withdraw_cash(account_balance)
     else:
         pass
 
 
 if __name__ == '__main__':
-    make_a_withdrawal()
+    # set_account_balance(100)
+    # set_correct_pin(1234)
+    make_a_withdrawal(correct_pin=1234, account_balance=100)
+
